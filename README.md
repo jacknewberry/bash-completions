85266d3d6fd760ac6c89abf4417a9a07fcb112af
Jack Newberry <jack.newberry@gmail.com>
2017-10-04 18:29:07 +0100

    Add completion script which will be used for fly_login

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bash-completions
+
+These completions scripts need to be found by bash. If using bash-it, this is achieved by sourcing completions in ~/.bash_it/custom/custom.completion.bash
+(see https://github.com/Bash-it/bash-it#your-custom-scripts-aliases-themes-and-functions)
+
+# resources:
+https://www.gnu.org/software/bash/manual/html_node/Programmable-Completion-Builtins.html#Programmable-Completion-Builtins
---
+++ _fly_login.bash
@@ -0,0 +1,39 @@
+
+_fly_login()
+{
+    local cur_word prev_word type_list
+
+    # COMP_WORDS is an array of words in the current command line.
+    # COMP_CWORD is the index of the current word (the one the cursor is
+    # in). So COMP_WORDS[COMP_CWORD] is the current word; we also record
+    # the previous word here, although this specific script doesn't
+    # use it yet.
+    cur_word="${COMP_WORDS[COMP_CWORD]}"
+    prev_word="${COMP_WORDS[COMP_CWORD-1]}"
+
+    # Ask pss to generate a list of types it supports
+    completion_list="abc abcd abcz abcdefg zumba" #`fly_login --get-completions`
+
+    # Only perform completion if the current word starts with a dash ('-'),
+    # meaning that the user is trying to complete an option.
+    # if [[ ${cur_word} == -* ]] ; then
+    #     # COMPREPLY is the array of possible completions, generated with
+    #     # the compgen builtin.
+    # else
+    #     #COMPREPLY=()
+    # fi
+
+    # Only show completions is this is the first argument
+    if [[ ${prev_word} == "fly_login" ]] ; then
+        # COMPREPLY is the array of possible completions, generated with
+        # the compgen builtin.
+        COMPREPLY=( $(compgen -W "${completion_list}" -- ${cur_word}) )
+    else
+        COMPREPLY=()
+    fi
+
+    return 0
+}
+
+# Register this completion function for the appropriate command(s)
+complete -F _fly_login fly_login
